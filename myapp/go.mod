ebc746ee3768875df9f4e5cab279737d6d3a0d7a
dongsheng <zhudongsheng@joyseasy.com>
2022-09-20 10:07:45 +0800

    +

--- myapp/go.mod
+++ myapp/go.mod
@@ -2,4 +2,7 @@ module myapp
 
 go 1.15
 
-require github.com/gogf/gf/v2 v2.1.4
+require (
+	github.com/gogf/gf/contrib/drivers/mysql/v2 v2.1.4
+	github.com/gogf/gf/v2 v2.1.4
+)
--- myapp/main.go
+++ myapp/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	_ "github.com/gogf/gf/contrib/drivers/mysql/v2"
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gcfg"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/glog"
+	"myapp/internal/model/entity"
 	_ "myapp/internal/packed"
 )
 
@@ -15,7 +17,8 @@ func main() {
 	//cmd.Main.Run(gctx.New())
 	//configTest()
 	//logTest()
-	errTest()
+	//errTest()
+	testORM()
 }
 func configTest() {
 	//单例获取配置文件对象
@@ -36,4 +39,28 @@ func logTest() {
 }
 func errTest() {
 	fmt.Printf("%v", gerror.New("ss"))
+}
+func testORM() {
+	mod := g.DB().Model("platNumber")
+
+	v, _ := mod.Clone().Limit(10).All()
+	fmt.Printf("platNumber:  %v \n", v)
+	//链式安全操作
+	v, _ = mod.Clone().Safe().Limit(10).All()
+	fmt.Printf("platNumber:  %v \n", v)
+	//写入
+	pn := &entity.Platnumber{
+		Platnid:   1212,
+		Platn:     "12343444",
+		NnameCN:   "test2",
+		NnameEN:   "test2",
+		Ndescript: " s",
+		Nvalid:    1,
+	}
+	mod.Clone().Data(pn).InsertAndGetId()
+	mod.Clone().Data(pn).Insert()
+	mod.Clone().Data(pn).InsertIgnore()
+	mod.Clone().Data(pn).Replace()
+	mod.Clone().Data(pn).OnDuplicate("nnameCN,nnameen").Save()
+
 }
